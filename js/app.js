dbf9f3ba3a6d8f8c8055aec9420e865662332a0a
nallely2 <nallelymd15gdelacruz@gmail.com>
2022-09-17 16:14:44 -0500

    jul

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Pregunta al usuario su nombre
+const nombre = prompt('Cual es tu nombre?')
+
+//Toma nombre y lo muestra
+document.querySelector('.pregunta') .innerHTML = `Hola ${nombre}, estas visitando la Bitácora de Jul`;
